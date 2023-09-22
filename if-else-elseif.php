e027afba0d3638631044aed4769813d78b02986b
korius kussa wenda <koriuswenda@gmail.com>
2023-09-22 16:35:36 +0700

    if-else-elseif.php

---
+++ if-else-elseif.php
@@ -0,0 +1,43 @@
+<?php
+
+$umur =20;
+
+if($umur > 17)
+{
+    echo " sudah dewasa";
+}
+
+
+// if, else = dua kondisi
+
+if($umur > 17 )
+{
+    echo "sudah dewasa";
+
+}else{
+    echo "<br> belum dewasa";
+}
+
+
+// if, else, else
+
+$nilai = 80;
+if($nilai > 20 )
+{
+    echo "<br> nilai a";
+
+}elseif($nilai > 80)
+{
+        echo"<br> nilai b ";
+}
+elseif($nilai > 70)
+{
+    echo"<br> nilai c ";
+}
+elseif($nilai > 60 )
+{
+    echo"<br> nilai d ";
+}
+
+
+?>
