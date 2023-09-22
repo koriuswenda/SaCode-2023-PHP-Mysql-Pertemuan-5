3872ccce351506656a8d4dec73c1a4015b15e65e
korius kussa wenda <koriuswenda@gmail.com>
2023-09-22 17:21:48 +0700

    loops

---
+++ do-while.php
@@ -0,0 +1,8 @@
+<?php
+
+$x = 1 ;
+
+do{
+    echo "pertemuan" . $x . "<br>";
+    $X++;
+}while($x <= 5);
---
+++ foreach.php
@@ -0,0 +1,7 @@
+<?php
+$warna = [ 'putih', 'merah','biru','kuning'];
+
+foreach($warna as $w)
+{
+        echo "warna : " .  $w . "<br>" ;
+}
